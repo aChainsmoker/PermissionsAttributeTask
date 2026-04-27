8eb09174be042c4efea2afa10fbd2618cb7bc0f9
Ivan <neborsky.ivan@tut.by>
2026-04-27 03:20:17 +0300

    feat: add request timing middleware

---
+++ PermissionsAttributeTask.Main/Extensions/Middleware/RequestTimeTrackingMiddlewareExtension.cs
@@ -0,0 +1,11 @@
+﻿using PermissionsAttributeTask.Main.Middleware;
+
+namespace PermissionsAttributeTask.Main.Extensions.Middleware;
+
+public static class RequestTimeTrackingMiddlewareExtension
+{
+    public static IApplicationBuilder UseRequestsTiming(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<RequestTimeTrackingMiddleware>();
+    }
+}
--- PermissionsAttributeTask.Main/Program.cs
+++ PermissionsAttributeTask.Main/Program.cs
@@ -1,64 +1,89 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using NLog;
+using NLog.Web;
 using PermissionsAttributeTask.DataAccess.DbContext;
 using PermissionsAttributeTask.DataAccess.Repositories;
 using PermissionsAttributeTask.DataAccess.Repositories.Abstractions;
 using PermissionsAttributeTask.DataAccess.Seeding;
 using PermissionsAttributeTask.Main.Configurations;
+using PermissionsAttributeTask.Main.Extensions.Middleware;
+using PermissionsAttributeTask.Main.Middleware;
 using PermissionsAttributeTask.Main.Services;
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 
-builder.Services.AddControllersWithViews();
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<PermissionsAttributeTaskDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnectionString"))
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+
+    builder.Services.AddControllersWithViews();
+
+    builder.Services.AddDbContext<PermissionsAttributeTaskDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnectionString"))
     );
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-    .AddEntityFrameworkStores<PermissionsAttributeTaskDbContext>()
-    .AddDefaultTokenProviders();
+    builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+        .AddEntityFrameworkStores<PermissionsAttributeTaskDbContext>()
+        .AddDefaultTokenProviders();
 
-builder.Services.AddScoped<IPermissionService, PermissionService>();
-builder.Services.AddScoped<IProfilesRepository, ProfileRepository>();
-builder.Services.AddScoped<DbSeeder>();
+    builder.Services.AddScoped<IPermissionService, PermissionService>();
+    builder.Services.AddScoped<IProfilesRepository, ProfileRepository>();
+    builder.Services.AddScoped<DbSeeder>();
 
-builder.Services.Configure<PermissionRolesSettings>(builder.Configuration.GetSection("PermissionRolesSettings"));
+    builder.Services.Configure<PermissionRolesSettings>(builder.Configuration.GetSection("PermissionRolesSettings"));
 
-var app = builder.Build();
+    var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbSeeder = scope.ServiceProvider.GetRequiredService<DbSeeder>();
+        try
+        {
+            await dbSeeder.Seed();
+        }
+        catch (Exception ex)
+        {
+            logger.Error(ex, "An error occurred while seeding the database.");
+        }
+    }
+
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseHttpMethodOverride();
+
+    app.UseRouting();
+    
+    app.UseRequestsTiming();
+    
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
+}
+catch (Exception exception)
 {
-    var dbSeeder = scope.ServiceProvider.GetRequiredService<DbSeeder>();
-    try
-    {
-        await dbSeeder.Seed();
-    }
-    catch (Exception ex)
-    {
-        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "An error occurred while seeding the database.");
-    }
+    logger.Error(exception, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    LogManager.Shutdown();
 }
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseHttpMethodOverride();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
